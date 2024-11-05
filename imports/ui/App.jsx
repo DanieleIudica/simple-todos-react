1b49fd12f96366e49b26549e6891cefa22404b1b
danieleiudica <daniele.iudica@freedompro.eu>
2024-11-05 23:29:48 +0100

    wip - 6.5: Pending tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Task } from "/imports/ui/Task.jsx";
 import { useSubscribe, useTracker } from "meteor/react-meteor-data";
 import { TasksCollection } from "/imports/api/TasksCollection";
 import { TaskForm } from "/imports/ui/TaskForm";
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const [hideCompleted, setHideCompleted] = useState(false);
+
+  const handleToggleChecked = ({ _id, isChecked }) =>
+    Meteor.callAsync("tasks.toggleChecked", { _id, isChecked });
+
+  const handleDelete = ({ _id }) => Meteor.callAsync("tasks.delete", { _id });
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const tasks = useTracker(() =>
+    TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 },
+    }).fetch()
+  );
+
   const isLoading = useSubscribe("tasks");
 
   if (isLoading()) {
@@ -13,16 +26,32 @@ export const App = () => {
   }
 
   return (
-    <div>
-      <h1>Welcome to Meteor!</h1>
-
-      <TaskForm />
-
-      <ul>
-        {tasks.map((task) => (
-          <Task key={task._id} task={task} />
-        ))}
-      </ul>
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>📝️ To Do List</h1>
+          </div>
+        </div>
+      </header>
+      <div className="main">
+        <TaskForm />
+        <div className="filter">
+          <button onClick={() => setHideCompleted(!hideCompleted)}>
+            {hideCompleted ? "Show All" : "Hide Completed"}
+          </button>
+        </div>
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={handleToggleChecked}
+              onDeleteClick={handleDelete}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
